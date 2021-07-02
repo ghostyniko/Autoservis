92ff273b1fa3b0cfeac4c99d922f4fadca39bbd9
ghostyniko <ghosyniko@gmail.com>
2021-07-02 14:29:56 +0200

    Remove Autoservis directory

--- Autoservis/Autoservis.DAL/Database.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Autoservis.DAL
-{
-  public static class Database
-  {
-    // naziv ključa konekcije na bazu podataka
-    public static readonly string ProjektConnectionString = "AutoservisDATAContainer";
-  }
-}
